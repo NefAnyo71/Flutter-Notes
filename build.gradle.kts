34dedb8892739b73cd8a36c9f992f46ed780ac0e
Arif Özdemir <arifkerem71@gmail.com>
2025-10-12 13:06:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // Google Services eklentisini uygula
     id("com.google.gms.google-services")
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -21,10 +20,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        // google-services.json ile eşleşmesi için applicationId güncellendi.
-        applicationId = "com.arif2.com"
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
+        applicationId = "com.appname.com"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -45,13 +41,7 @@ flutter {
 }
 
 dependencies {
-    // Firebase BoM (Bill of Materials) import et.
-    // Bu, Firebase kütüphanelerinin uyumlu sürümlerini otomatik olarak yönetir.
     implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
-
-    // Kullanmak istediğiniz Firebase ürünleri için bağımlılıkları ekleyin.
-    // BoM kullanırken, Firebase bağımlılıklarında sürüm belirtmeyin.
     implementation("com.google.firebase:firebase-analytics")
-
-    // Firestore için gerekli olan cloud_firestore paketi zaten Flutter tarafında eklendiği için buraya eklemeye gerek yoktur.
 }
+
